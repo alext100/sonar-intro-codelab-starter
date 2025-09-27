506a1ecdd745b565d86d3a0eb87feaa5fbabcd15
Aleksandr Turchenko <alexandrt100@gmail.com>
2025-09-27 18:43:56 +0200

    Add test

--- src/index.test.js
+++ src/index.test.js
@@ -3,3 +3,19 @@ import { sortAnimals } from ".";
 it("GIVEN sortAnimals, WHEN is invoked THEN should return even length animals list", () => {
   expect(sortAnimals().length % 2).toBe(0);
 });
+
+it("GIVEN sortAnimals, WHEN is invoked THEN should return sorted animals list", () => {
+  const sortedAnimals = [
+    { animal: "cat", type: "mammal" },
+    { animal: "dog", type: "mammal" },
+    { animal: "dolphin", type: "mammal" },
+    { animal: "eagle", type: "bird" },
+    { animal: "elephant", type: "mammal" },
+    { animal: "frog", type: "amphibian" },
+    { animal: "lion", type: "mammal" },
+    { animal: "penguin", type: "bird" },
+    { animal: "shark", type: "fish" },
+    { animal: "snake", type: "reptile" },
+  ];
+  expect(sortAnimals()).toEqual(sortedAnimals);
+});
