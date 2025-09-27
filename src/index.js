25e74ccbcba70e8916b8e4319b680b3f172f6b90
Aleksandr Turchenko <alexandrt100@gmail.com>
2025-09-27 16:22:31 +0200

    Add sorting function

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const animalsData = [
 
 export function sortAnimals() {
   //This will trigger a Bug warning
-  animalsData.sort();
+  animalsData.sort((a, b) => a.animal.localeCompare(b.animal));
 
   //This will trigger a Security Hotspot warning
   const randomInt = Math.floor(2 * Math.random());
